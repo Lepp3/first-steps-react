800ab8726fd1eb863fbca35044bbbee656561106
Lepp3 <lubaka05@gmail.com>
2025-02-18 19:25:09 +0200

    first three components added, removed default css

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer(){
+    return(
+        <footer><p>First React project footer</p></footer>
+    )
+}
+
+
+export default Footer
---
+++ src/components/Main.jsx
@@ -0,0 +1,8 @@
+function Main(){
+    return(
+        <main>Main section</main>
+    )
+}
+
+
+export default Main
