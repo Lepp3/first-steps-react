f8f1559b3ab66a1668343aa7a34d9ee5af8503f8
Lepp3 <lubaka05@gmail.com>
2025-02-19 23:27:55 +0200

    refactored for better readability

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,12 +1,13 @@
 import MovieListItem from "./MovieListItem";
 
 export default function MovieList(props){
+    
     return(
         <>
         <h3>Movie List</h3>
-        <ul>
-            <MovieListItem title={props.movies[0].title}/>
-        </ul>
+        <div>
+            <MovieListItem movie={props.movies[0]}/>
+        </div>
         </>
     );
 }
--- src/components/MovieListItem.jsx
+++ src/components/MovieListItem.jsx
@@ -1,7 +1,16 @@
 export default function MovieListItem(props){
     return(
-        <li>
-            {props.title}
-        </li>
+        <article>
+            <header>
+                <h4>{props.movie.title}</h4>
+            </header>
+            <main>
+            <p>{props.movie.plot}</p>
+            </main>
+            <footer>
+                <p>Director: {props.movie.director}, year: {props.movie.year}</p>
+            </footer>
+            
+        </article>
     )
 }
