98b896b2185b66e41f3f3fb08633db65b440eb5e
Lepp3 <lubaka05@gmail.com>
2025-02-22 21:43:28 +0200

    removed index option for keys to avoid matching keys when rendering

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -7,7 +7,7 @@ export default function MovieList(props){
         <h3>Movie List</h3>
         <div>
             {props.movies.map((movie, index)=>(
-                <MovieListItem key={movie.id || index} movie={movie}/>
+                <MovieListItem key={movie.id} movie={movie}/>
             ))}
             
             
