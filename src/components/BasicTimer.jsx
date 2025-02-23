746f014c11a454ca95ddd32e3ce3d491943bd8e0
lmilanov <lubaka05@gmail.com>
2025-02-23 23:14:43 +0200

    implemented add time effect

--- src/components/BasicTimer.jsx
+++ src/components/BasicTimer.jsx
@@ -1,14 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 export default function Timer(){
     const startingValue = 0;
+    const [isManual,setManual] = useState(false);
     const [time,setTime] = useState(startingValue);
     
     setTimeout(() => {
         setTime(time+1);
     }, 1000);
 
+    useEffect(()=>{
+        setTimeout(() => {
+            setTime(seconds => seconds +1);
+            setManual(false);
+        }, 1000);
+    })
+
+
+    const addTimeHandler = () => {
+        setTime(seconds => seconds +10);
+        setManual(true);
+    }
 
     return(
         <>
