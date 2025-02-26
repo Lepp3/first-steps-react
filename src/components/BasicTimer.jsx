3e31b78876852d8cb5111c012b2e9234d8a23f24
Lepp3 <lubaka05@gmail.com>
2025-02-26 14:26:18 +0200

    fixed basic timer add seconds implementation so it doesn't multiply settimeouts

--- src/components/BasicTimer.jsx
+++ src/components/BasicTimer.jsx
@@ -7,27 +7,28 @@ export default function Timer(){
     const [time,setTime] = useState(()=>{
         return new Date().getSeconds()});
     
-    setTimeout(() => {
-        setTime(seconds => seconds + 1);
-    }, 1000);
+  
 
-    // useEffect(()=>{
-    //     setTimeout(() => {
-    //         setTime(seconds => seconds +1);
-    //         setManual(true);
-    //     }, 1000);
-    // })
+    useEffect(()=>{
+        if(!isManual){
+            setTimeout(() => {
+                setTime(seconds => seconds + 1);
+            }, 1000); 
+            setManual(false);
+        }
+    },[time,isManual])
 
 
     const addTimeHandler = () => {
-        setTime(seconds => seconds +10);
-        setManual(true);
+        setTime(seconds => seconds + 10);
+        
     }
 
     return(
         <>
         <h2>Timer</h2>
         <div>{time}</div>
+        <button onClick={addTimeHandler}>Add time</button>
         <hr/>
         </>
     )
